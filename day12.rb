05d90341cde77d66daeff6fd01eb4af41795074a
Paras Sanghavi <insomniac18@gmail.com>
2020-12-12 15:37:35 -0800

    day12

---
+++ day12.rb
@@ -0,0 +1,85 @@
+require 'pry'
+require "matrix"
+file = File.read('./inputs/day12')
+
+@rows = file.split("\n").map do |row|
+    chars = row.split('')
+    [chars[0], chars[1..].join('').to_i]
+end
+
+def part1
+    pos = Vector[0, 0]
+    direction = 0
+
+    @rows.each do |instruction|
+        cmd, amount = instruction
+        case cmd
+        when 'N'
+            pos += amount * Vector[0, 1]
+        when 'S'
+            pos += amount * Vector[0, -1]
+        when 'E'
+            pos += amount * Vector[1, 0]
+        when 'W'
+            pos += amount * Vector[-1, 0]
+        when 'L'
+            direction += amount
+        when 'R'
+            direction -= amount
+        when 'F'
+            radians = direction * Math::PI/180
+            pos += amount * Vector[Math.cos(radians).to_i, Math.sin(radians).to_i]
+        end
+    end
+
+    puts pos.map(&:abs).sum
+    puts pos.map(&:abs).sum == 2270
+end
+
+# part1
+
+def part2
+    pos = Vector[0.0, 0.0]
+    w = Vector[10.0, 1.0]
+
+    @rows.each do |instruction|
+        cmd, amount = instruction
+        case cmd
+        when 'N'
+            w += amount * Vector[0, 1]
+        when 'S'
+            w += amount * Vector[0, -1]
+        when 'E'
+            w += amount * Vector[1, 0]
+        when 'W'
+            w += amount * Vector[-1, 0]
+        when 'L'
+            existing_angle = if w[1].positive?
+                w.angle_with(Vector[1,0])
+            elsif w[0].positive?
+                -w.angle_with(Vector[1,0])
+            elsif w[0].negative?
+                2 * Math::PI - w.angle_with(Vector[1,0])
+            end
+            angle = existing_angle + (amount * Math::PI/180)
+            w = w.magnitude * Vector[Math::cos(angle), Math::sin(angle)]
+        when 'R'
+            existing_angle = if w[1].positive?
+                w.angle_with(Vector[1,0])
+            elsif w[0].positive?
+                -w.angle_with(Vector[1,0])
+            elsif w[0].negative?
+                2 * Math::PI - w.angle_with(Vector[1,0])
+            end
+            angle = existing_angle - (amount * Math::PI/180)
+            w = w.magnitude * Vector[Math::cos(angle), Math::sin(angle)]
+        when 'F'
+            pos += amount * w
+        end
+    end
+
+    puts pos.map(&:abs).sum
+    puts pos.map(&:abs).sum.round(2) == 138669
+end
+
+part2
